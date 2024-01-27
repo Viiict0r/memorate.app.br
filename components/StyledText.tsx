55c48ed1aab5b16df50b1f1e4702ea8e370b8e6a
Viiict0r <victorh.cepil@hotmail.com>
2024-01-27 13:15:24 -0300

    feat: updated StyledText component to work with added font

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,5 +1,62 @@
-import { Text, TextProps } from './Themed';
+import { Text, TextProps } from '@/components/Themed';
 
-export function MonoText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: 'SpaceMono' }]} />;
+type Props = {
+  size?: number;
+} & TextProps;
+
+export function TextLight({ size, ...props }: Props) {
+  return (
+    <Text
+      {...props}
+      style={[
+        props.style,
+        { fontFamily: 'PoppinsLight', fontSize: size || 14 },
+      ]}
+    />
+  );
+}
+
+export function TextRegular({ size, ...props }: Props) {
+  return (
+    <Text
+      {...props}
+      style={[
+        props.style,
+        { fontFamily: 'PoppinsRegular', fontSize: size || 14 },
+      ]}
+    />
+  );
+}
+
+export function TextMedium({ size, ...props }: Props) {
+  return (
+    <Text
+      {...props}
+      style={[
+        props.style,
+        { fontFamily: 'PoppinsMedium', fontSize: size || 14 },
+      ]}
+    />
+  );
+}
+
+export function TextSemiBold({ size, ...props }: Props) {
+  return (
+    <Text
+      {...props}
+      style={[
+        props.style,
+        { fontFamily: 'PoppinsSemiBold', fontSize: size || 14 },
+      ]}
+    />
+  );
+}
+
+export function TextBold({ size, ...props }: Props) {
+  return (
+    <Text
+      {...props}
+      style={[props.style, { fontFamily: 'PoppinsBold', fontSize: size || 14 }]}
+    />
+  );
 }
